6990b5637dd8b838af4f5b5ae2619e375c85b682
vicky2222-2 <vigneshwarank2222@gmail.com>
2024-06-23 11:49:52 +0530

    nothing
    nothing

--- Readme.md
+++ Readme.md
@@ -1,1 +1,19 @@
-hello github, i am vignesh
+# In this project I am going to build silly name generator
+
+ credits goes to improtical python project book because I learned this program from this book.
+
+
+## pseducode for clarity
+
+#### 1.Load a list of first names
+#### 2.Load a list of surnames
+#### 3.Choose a first name at random
+#### 4.Assign the name to a variable
+#### 5.Choose a surname at random
+#### 6.Assign the name to a variable
+#### 7.Print the names to the screen in order and in red font
+#### 8.Ask the user to quit or play again
+#### 9.   If user plays again:
+#### 10.      repeat
+#### 11.  If user quits:
+#### 12       end and exit
---
+++ main.py
@@ -0,0 +1,60 @@
+
+""" Generating sillyname by combing the two names from tuple """
+
+
+import sys
+import random
+
+def main():
+    """ Selecting the two names randomly from the tuples """
+    print("welcome to the silly name generator:")
+    print("\n In here may be you can see your name also\n")
+
+    first = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'",
+             "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite' ",
+             'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield',
+             'Chewy', 'Chigger", "Cinnabuns', 'Cleet', 'Cornbread', 'Crab Meat',
+             'Crapps', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
+             'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
+             'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny',
+             'Lemongrass', 'Lil Debil', 'Longbranch', '"Lunch Money"',
+             'Mergatroid', '"Mr Peabody"', 'Oil-Can', 'Oinks', 'Old Scratch',
+             'Ovaltine', 'Pennywhistle', 'Pitchfork Ben', 'Potato Bug',
+             'Pushmeet', 'Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
+             "Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs',
+             'Snorki', 'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky',
+             'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
+             "Winston', 'Jazz Hands'", 'Worms')
+
+    last = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom',
+            'Breedslovetrout', 'Butterbaugh', 'Clovenhoof', 'Clutterbuck',
+            'Cocktoasten', 'Endicott', 'Fewhairs', 'Gooberdapple', 'Goodensmith',
+            'Goodpasture', 'Guster', 'Henderson', 'Hooperbag', 'Hoosenater',
+            'Hootkins', 'Jefferson', 'Jenkins', 'Jingley-Schmidt', 'Johnson',
+            'Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles',
+            'Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf',
+            'Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
+            'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
+            'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern',
+            'Stevens', 'Stroganoff', 'Sugar-Gold', 'Swackhamer', 'Tippins',
+            'Turnipseed', 'Vinaigrette', 'Walkingstick', 'Wallbanger', 'Weewax',
+            'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
+            'Woolysocks')
+
+    while True:
+
+        first_name = random.choice(first)
+        last_name = random.choice(last)
+
+        # using IDLE trick to get the red colour by printing "fatal error"
+        print("\n\n")
+        print(f"{first_name} {last_name}".format(first_name, last_name), file=sys.stderr)
+        print("\n\n")
+
+        tryagain = input("\n press enter to tryagain (press n for quit):")
+        if tryagain.lower() == 'n':
+            break
+
+    input("\nPress Enter")
+if __name__ == '__main__':
+    main()
