57679a448f0a9b9b9df9e89d64d3e67da34564cd
vicky2222-2 <vigneshwarank2222@gmail.com>
2024-06-24 08:42:23 +0530

    Update main.py

--- anargms_phase/main.py
+++ anargms_phase/main.py
@@ -1,17 +1,4 @@
 """ convent the word it in to differ meaning words with same letters"""
-#psuedocode for clarity
-
-# 1. load the dictionary
-# 2. convert it in to word_list
-# 3. empty list for holding anargms
-# 4. Get the word from the user and make it lower for use
-# 5. for word in word_list
-# 6.    sorted the word
-# 7.    if the word not equal to user_word:
-# 8.         if sorted_user_word equal to sorted_word
-# 9.              add the word to the empty list anargms
-# 10. printing the anargms
-
 
 #importing load dictionary for converting the text file into the list of words
 import load_dictionary
